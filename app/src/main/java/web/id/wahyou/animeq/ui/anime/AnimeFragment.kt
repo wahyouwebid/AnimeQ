8d6165397f870ac491ce8f887b6a8f7f5ca63c71
ujang <ujang@gmail.com>
2021-02-09 23:48:20 +0700

    (fix) Lint infrastructure error

--- app/src/main/java/web/id/wahyou/animeq/ui/anime/AnimeFragment.kt
+++ app/src/main/java/web/id/wahyou/animeq/ui/anime/AnimeFragment.kt
@@ -7,7 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.observe
 import androidx.recyclerview.widget.GridLayoutManager
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
